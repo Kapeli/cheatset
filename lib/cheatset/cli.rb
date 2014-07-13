bbed17a91e5646902cfe3c51ce512c34d1924aa6
Kapeli <support@kapeli.com>
2014-07-13 04:07:55 +0300

    Support latest Rouge (syntax highlighting gem)

--- lib/cheatset/cli.rb
+++ lib/cheatset/cli.rb
@@ -1,5 +1,17 @@
 require 'thor'
 
+module Rouge
+    module Formatters
+        class HTML < Formatter
+            def stream_untableized(tokens, &b)
+              yield "<pre#@css_class>" if @wrap
+              tokens.each{ |tok, val| span(tok, val, &b) }
+              yield "</pre>\n" if @wrap
+            end
+        end
+    end
+end
+
 module Cheatset
   class CLI < Thor
     desc 'generate FILE', 'Generates cheatsheet out of a file'

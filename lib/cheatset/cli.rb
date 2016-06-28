3bcec5ecd4eb59a51e0b4d47e276f16b8fee6fa9
Kapeli <support@kapeli.com>
2016-06-28 17:05:53 +0300

    Add support for latest Rouge

--- lib/cheatset/cli.rb
+++ lib/cheatset/cli.rb
@@ -3,10 +3,17 @@ require 'thor'
 module Rouge
     module Formatters
         class HTML < Formatter
-            def stream_untableized(tokens, &b)
+            def stream_untableized(tokens, &b) # prevent Rouge from using <pre><code>, we just want <pre>
               yield "<pre#@css_class>" if @wrap
               tokens.each{ |tok, val| span(tok, val, &b) }
               yield "</pre>\n" if @wrap
+            end
+        end
+        class HTMLPygments < Formatter
+            def stream(tokens, &b) # prevent Rouge from using <pre><code>, we just want <pre>
+              yield %<<pre class="#@css_class">>
+              @inner.stream(tokens, &b)
+              yield "</pre>"
             end
         end
     end
